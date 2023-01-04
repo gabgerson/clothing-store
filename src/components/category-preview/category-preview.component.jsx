fcb9b4a16bb88a723e7d6e16496be770fd31f45e
Gabriela Gerson <gabgerson1@gmail.com>
2023-01-04 12:55:05 -0800

    Finish Category Page

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import ProductCard from '../product-card/product-card.component'
 
 import './category-preview.styles.scss'
@@ -6,7 +8,13 @@ const CategoryPreview = ({ title, products}) => {
   return (
     <div className='category-preview-container'>
       <h2>
-        <span className='title'>{title.toUpperCase()}</span>
+        {/* <span className='title'>{title.toUpperCase()}</span> */}
+
+        <Link  className='nav-link' to={title}>
+          <span className='title'>{title.toUpperCase()}</span>
+        </Link>
+        
+
       </h2>
       <div className='preview'>
         {
